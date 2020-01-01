8b5d313ca989322b24ae767e8a403a894456cccf
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-01-01 10:12:53 -0600

    implement SignInAndSignOut Page

--- My-project/poridhan/src/App.js
+++ My-project/poridhan/src/App.js
@@ -4,6 +4,8 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+
+import SignInAndSignOutPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
 import Header from './components/header/header.component';
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignOutPage} />
       </Switch>
     </div>
   );
---
+++ My-project/poridhan/src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sign-in-and-sign-out.styles.scss';
+
+const SignInAndSignOutPage = () => (
+  <div className="sign-in-and-sign-out">
+    <h1 className="title">I already have an account</h1>
+    <span className="subtitle">Sign in with your emai and password</span>
+    <div>Sign in page</div>
+  </div>
+);
+
+export default SignInAndSignOutPage;
