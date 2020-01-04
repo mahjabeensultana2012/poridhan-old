e87b2143ca9477526ef509156b670178316db33d
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-01-03 18:11:52 -0600

    implement firebase.utils with google auth

--- My-project/poridhan/src/firebase/firebase.utils.js
+++ My-project/poridhan/src/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import 'firebase/storage';
+import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
@@ -14,3 +14,13 @@ const config = {
 };
 
 firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
