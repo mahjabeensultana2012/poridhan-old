83f1ab070429060f2f5df237a1bfedb1765b4297
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-01-06 10:48:26 -0600

    implement sign-up component

--- My-project/poridhan/src/components/sign-up/sign-up.component.jsx
+++ My-project/poridhan/src/components/sign-up/sign-up.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 import './sign-up.styles.scss';
 
 class SignUp extends React.Component {
@@ -14,6 +15,39 @@ class SignUp extends React.Component {
       confirmPassword: '',
     };
   }
+
+  handleSubmit = async event => {
+    event.PreventDefault();
+
+    const { displayName, email, password, confirmPassword } = this.state;
+    if (password !== confirmPassword) {
+      alert("passwords don't match");
+      return;
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      await createUserProfileDocument(user, { displayName });
+
+      this.setState({
+        displayName: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
 
   render() {
     const { displayName, email, password, confirmPassword } = this.state;
@@ -57,8 +91,11 @@ class SignUp extends React.Component {
             label="Confirm Password"
             required
           />
+          <CustomButton type="submit">SIGN UP</CustomButton>
         </form>
       </div>
     );
   }
 }
+
+export default SignUp;
