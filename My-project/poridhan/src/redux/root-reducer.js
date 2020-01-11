b1e50abfce5037de37b47e2628329bb2c8094fbf
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-01-10 18:49:33 -0600

    toggle cart hidden is working in header component

--- My-project/poridhan/src/redux/root-reducer.js
+++ My-project/poridhan/src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user-reducer';
+import cartReducer from './cart/cart.reduer';
 
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
